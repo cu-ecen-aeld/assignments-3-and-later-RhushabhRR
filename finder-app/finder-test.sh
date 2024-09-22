49a578faff3fbae328d52c850be78147165322a1
RhushabhRR <rhushabhraghavendrarr@gmail.com>
2024-09-22 09:15:10 +0000

    finder-test script modified for assignment 4

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -5,6 +5,8 @@
 set -e
 set -u
 
+cd /etc/finder-app/
+
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
@@ -54,10 +56,12 @@ fi
 
 for i in $( seq 1 $NUMFILES)
 do
-	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
+
+writer "/tmp/assignment4-result.txt" "$OUTPUTSTRING"
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
